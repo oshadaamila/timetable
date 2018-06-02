1a63b7be3107bdd08842fe825e585e18d22aefa0
oshadaamila <oshadaamila.15@cse.mrt.ac.lk>
2018-06-02 16:33:23 +0530

    opening_file_done

--- src/timetable/Timetable.java
+++ src/timetable/Timetable.java
@@ -5,6 +5,12 @@
  */
 package timetable;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author pavilion 15
@@ -14,8 +20,31 @@ public class Timetable {
     /**
      * @param args the command line arguments
      */
+    static ArrayList<String[]> input= new ArrayList<String[]>();
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Enter input file path: ");
+        Scanner scanner = new Scanner(System.in);
+        String inputFilePath = scanner.nextLine();
+        System.out.println("Enter output file path: ");
+        String outputFilePath = scanner.nextLine();
+        String text="";
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFilePath))) {
+            while ((text = br.readLine()) != null) {
+                     // use comma as separator
+                input.add(text.split(","));
+                        
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for(String[] x:input){
+            for(String y:x)
+            System.out.println(y);
+        }
+        
+
     }
     
 }
