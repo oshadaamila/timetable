9deb5065549da4909e976c5834f7594b3ef8faad
oshadaamila <oshadaamila.15@cse.mrt.ac.lk>
2018-06-02 17:50:50 +0530

    put_to_array

--- src/timetable/Timetable.java
+++ src/timetable/Timetable.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -21,6 +22,8 @@ public class Timetable {
      * @param args the command line arguments
      */
     static ArrayList<String[]> input= new ArrayList<String[]>();
+    static ArrayList<Subject> subjectList= new ArrayList<Subject>();
+    static ArrayList<String> rooms= new ArrayList<String>();
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Enter input file path: ");
@@ -39,12 +42,32 @@ public class Timetable {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        for(String[] x:input){
-            for(String y:x)
-            System.out.println(y);
+        for(int i=0;i<input.size()-1;i++){
+            Subject sbj = getSubject(input.get(i));
+            subjectList.add(sbj);
+        }
+        for(String x:input.get(input.size()-1)){
+            rooms.add(x);
         }
         
+        System.out.println("finished");
+        
+        
 
+    }
+    
+    static Subject getSubject(String[] array){
+        String name = array[0];
+        boolean cmplsr;
+        System.out.println(array[1]);
+        if(array[1].toString().contains("c")){
+            cmplsr=true;
+        }else{
+            cmplsr=false;
+        }
+        String [] timeslts = Arrays.copyOfRange(array, 2, array.length);
+        Subject sbj =  new Subject(cmplsr,name,timeslts);
+        return sbj;
     }
     
 }
