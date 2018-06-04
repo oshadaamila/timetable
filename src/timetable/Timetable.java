1ef1032ad68fab17801cadda9556ff3d3f75c478
oshadaamila <oshadaamila.15@cse.mrt.ac.lk>
2018-06-04 18:58:37 +0530

    before_output_file

--- src/timetable/Timetable.java
+++ src/timetable/Timetable.java
@@ -23,7 +23,11 @@ public class Timetable {
      */
     static ArrayList<String[]> input= new ArrayList<String[]>();
     static ArrayList<Subject> subjectList= new ArrayList<Subject>();
+    static ArrayList<String> timeSlots = new ArrayList<>();
     static ArrayList<String> rooms= new ArrayList<String>();
+    static ArrayList<String> generatedAllTimeSlots = new ArrayList<>();
+    static ArrayList<String> compulsoryTimeSlots = new ArrayList<>();
+    static ArrayList<String> assignedTimeSlots = new ArrayList<>();
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Enter input file path: ");
@@ -52,11 +56,10 @@ public class Timetable {
             Subject sbj = getSubject(input.get(i));
             subjectList.add(sbj);
         }
+        generateAllTimeSlots();
+        assignTime(0);
         
-        
-        
-        
-        System.out.println("finished");
+        System.out.println("Test");
         
         
 
@@ -71,21 +74,16 @@ public class Timetable {
             cmplsr=false;
         }
         String [] timeslts = Arrays.copyOfRange(array, 2, array.length);
+        for (String x: timeslts){
+            if(!timeSlots.contains(x)){
+                timeSlots.add(x);
+            }
+        }
         Subject sbj =  new Subject(cmplsr,name,timeslts);
         return sbj;
     }
     
-    static Subject findSubjectWithMaximumPossibleTimeSlots(){
-        int max = 0;
-        Subject subject= null;
-        for(Subject sbj: subjectList){
-            if(sbj.getNoOfPossibleTimeSlots()>max){
-                max = sbj.getNoOfPossibleTimeSlots();
-                subject = sbj;   
-            }
-        }
-        return subject;
-    }
+    
     //check this
      static boolean AllAssigned(){
          boolean output=true;
@@ -96,6 +94,79 @@ public class Timetable {
          }
          return output;
      }
+     
+     static void assignTime(int index){
+         for(String timeSlot:generatedAllTimeSlots){
+             if(timeSlotIsAvailable(index,timeSlot)){
+                 if(subjectList.get(index).compulsory){
+                     subjectList.get(index).assignTimeSlot(timeSlot);
+                     assignedTimeSlots.add(timeSlot);
+                     addTimeSlotToCompulsory(timeSlot);
+                 }else{
+                     subjectList.get(index).assignTimeSlot(timeSlot);
+                     assignedTimeSlots.add(timeSlot);
+                 }
+                 if(index+1<subjectList.size()){
+                     assignTime(index+1);
+                 }else{
+                     System.out.println("shit");
+                     return;
+                 }
+                 
+             }
+         }
+         
+     }
+     
+     static void generateAllTimeSlots(){
+        for(String timeSlot : timeSlots){
+            for (String room : Timetable.rooms){
+                generatedAllTimeSlots.add(timeSlot+","+room);
+            }
+        }
+    }
+     
+    static boolean timeSlotIsAvailable(int index, String timeSlot){
+        boolean a = checkInCompulsory(timeSlot);
+        boolean b = checkTimeSlotIsAssigned(timeSlot);
+       boolean c =  subjectList.get(index).isAvailableOnTimeSlot(timeSlot);
+        
+        if(checkInCompulsory(timeSlot) && checkTimeSlotIsAssigned(timeSlot) && 
+                subjectList.get(index).isAvailableOnTimeSlot(timeSlot)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    static boolean checkInCompulsory( String timeSlot){ //return true if a givenTime is not in Compulsory
+        String [] timeArray = timeSlot.split(",");
+        String onlyTime =  timeArray[0];
+        if(compulsoryTimeSlots.contains(onlyTime)){
+            return false;
+        }else{
+            return true;
+        }  
+    }
+    
+    static boolean checkTimeSlotIsAssigned(String timeslot){        //return true if a givenTime is not in assigned time slots
+        if(assignedTimeSlots.contains(timeslot)){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    
+    static void addTimeSlotToCompulsory(String timeSlot){   //add time slot to compulsory time slot array
+        String [] timeArray = timeSlot.split(",");
+        String onlyTime =  timeArray[0];
+        compulsoryTimeSlots.add(onlyTime);
+    }
+    
+    
+        
+        
+        
          
     
 }
